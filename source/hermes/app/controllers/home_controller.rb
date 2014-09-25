4836e05f12c19f10a6b8287f0d3b1503a44b33cd
ruby <ruby@ruby>
2014-09-25 00:21:52 -0400

    Ahora se muestra el nombre al hacer login

--- source/hermes/app/controllers/home_controller.rb
+++ source/hermes/app/controllers/home_controller.rb
@@ -1,27 +1,30 @@
 class HomeController < ApplicationController
-	before_action :set_usuario
+	#before_action :set_usuario
 
 	def index
-		if params[:tipo] == 1		#Administrator	
-      		#habilitar cosas de administrador
-    	else						#Common User
-      	
-    	end
-
+		if session[:id_usuario_actual]
+			@usuario=Usuario.find(session[:id_usuario_actual])
+			if @usuario.tipo_usuario == 1		#Administrator	
+	      		#habilitar cosas de administrador
+	      		p 'administrador'
+	    	else						#Common User
+	      		p 'usuario comun'
+	    	end
+	    end
 	end
 
 	def login
 		begin
-			usuario = Usuario.find_by(correo_electronico: params[:login]).authenticate(params[:passwd])
+			@usuario = Usuario.find_by(correo_electronico: params[:login]).authenticate(params[:passwd])
 		rescue NoMethodError
 				#enviar mensaje ajax de correo inexistente
 				redirect_to root_url
 		else	
-		  	if usuario == false
+		  	if @usuario == false
 		  		#enviar mensaje ajax de contrase;a erronea
 		  		redirect_to root_url
 		  	else
-		  		session[:id_usuario_actual]=usuario.id
+		  		session[:id_usuario_actual]=@usuario.id
 		  		p session[:id_usuario_actual]
 		  		redirect_to root_url
 		  	end
