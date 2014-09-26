f8fac6b745a118b53a8da3eca3ecf47205ad942b
ruby <ruby@ruby>
2014-09-26 16:52:42 -0400

    Subiendo correcciones

--- source/hermes/app/controllers/password_controller.rb
+++ source/hermes/app/controllers/password_controller.rb
@@ -2,13 +2,18 @@ class PasswordController < ApplicationController
 	
 
 	def retrieve #envia correo para recuerpar la contrasena y regresa al main
+		begin
 			@usuario=Usuario.find_by(correo_electronico: params[:correo_electronico])
+
+			if @usuario.nil?
+				raise "Usuario no encontrado en el sistema"
+			end
+
 			UserMailer.retrieve_pwd(@usuario).deliver
 			redirect_to root_url
-	end
-
-	def ask
-		@usuario=Usuario.find_by(correo_electronico: params[:correo_electronico])
+		rescue Exception => e
+			raise e
+		end			
 	end
 
 	def answer  #maneja el resultado de la pregunta secreta y la respuesta
@@ -24,18 +29,22 @@ class PasswordController < ApplicationController
 		rescue
 			redirect_to root_url
 		end
-
 	end
 
 	def new_password
+	end
 
+	def ask
+		@usuario=Usuario.find_by(correo_electronico: params[:correo_electronico])
 	end
 
 	def update #maneja la actualizacion de la contrasena
 		begin
 			@usuario=Usuario.find_by(correo_electronico: params[:correo_electronico])
-			@usuario.password=params[contrasena]
+			@usuario.password=params[:contrasena]
 			@usuario.save
+			reset_session
+			redirect_to root_url
 		rescue
 			redirect_to root_url
 		end
