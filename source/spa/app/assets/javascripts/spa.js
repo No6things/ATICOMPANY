e7fc19a386c2e622d487c0fb1e58a8cd9cd15da6
ruby <ruby@ruby>
2014-10-23 18:09:29 -0400

    Subiendo datos de bd

--- source/spa/app/assets/javascripts/spa.js
+++ source/spa/app/assets/javascripts/spa.js
@@ -1,20 +1,26 @@
-var app = angular.module("spa", []);
-app.config(function($httpProvider) {
-      //Enable cross domain calls
-      $httpProvider.defaults.useXDomain = true;
-      //Remove the header used to identify ajax call  that would prevent CORS from working
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  });
+(function(){
 
-app.controller("loginController", function ($scope, $http){
-  var usuario = {
-    login: $scope.correo,
-    passwd: $scope.contrasena
-  };
-	$http.post("http://localhost:3000/login",usuario).success( function(response) {
-												$scope.usuario.nombre = response;
-											})
-											.error(function() {
-    											console.log("error");
-											});
-});
+  var app = angular.module("spa", []);
+  var remoteDomain = "http://localhost:3000/";
+
+  app.config(function($httpProvider) {
+        //Enable cross domain calls
+        $httpProvider.defaults.useXDomain = true;
+        //Remove the header used to identify ajax call  that would prevent CORS from working
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    });
+
+  app.controller("loginController", function ($scope, $http){
+    var usuario = {
+      login: $scope.correo,
+      passwd: $scope.contrasena
+    };
+    $http.post(remoteDomain+"login",usuario).success( function(response) {
+      $scope.usuario.nombre = response;
+    }).error(function(m) {
+      console.log(m);
+    });
+
+  });  
+
+})();
