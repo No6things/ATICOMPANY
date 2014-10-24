d280629aebd188388c94a2a1135dbdf3e6d278c2
ruby <ruby@ruby>
2014-10-23 22:53:10 -0400

    Subiendo controller

--- source/spa/app/assets/javascripts/operador.js
+++ source/spa/app/assets/javascripts/operador.js
@@ -1,5 +1,15 @@
 (function(){
 	
+	var app = angular.module('funciones-operadores', ['spa']);
+	var remoteDomain = "http://localhost:3000/";
 
-	
+	app.config(function($httpProvider) {
+	    $httpProvider.defaults.headers.common= {'enterprise-token': 1};
+	    $httpProvider.defaults.useXDomain = true;
+	    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	});
+
+
+
+
 })();
