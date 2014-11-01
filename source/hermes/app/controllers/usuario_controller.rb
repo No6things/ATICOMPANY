4ddd707ff26f195b342fa7d977b064d929f7ce4c
nacho <jesus.igp009@gmail.com>
2014-10-31 20:04:49 -0400

    Subiendo ejemplo de documentacion con yard

--- source/hermes/app/controllers/usuario_controller.rb
+++ source/hermes/app/controllers/usuario_controller.rb
@@ -1,6 +1,101 @@
 class UsuarioController < ApplicationController
 before_filter :check_api_token
 
+=begin
+
+@api Hermes 1.0
+
+@param email [String] correo electronico del usuario que ha 
+	enviado o recibido el paquete a listar
+
+@return [Json] representacion en formato json de los paquetes 
+	encontrados
+
+@note 
+	Parametro "api-token" requerido en cabecera http
+
+@example Ejemplo de json de retorno
+	{
+	    "err_mssg": "",
+	    "success_mssg": "OK",
+	    "data": {
+	        "como_emisor": [
+	            {
+	                "id": 15,
+	                "ancho": 5,
+	                "alto": 5,
+	                "peso": 0.15,
+	                "costo": 24.5,
+	                "profundidad": 10,
+	                "descripcion": "Teclado",
+	                "numero_guia": "1acae99bed11b1424527f4c930907fc498273739",
+	                "emisor": {
+	                    "id": 13,
+	                    "nombre": "Administrador",
+	                    "apellido": "Prueba",
+	                    "correo_electronico": "a@m.com",
+	                    "fecha_ultimo_acceso": "2014-09-27T00:18:14.693Z",
+	                    "tipo_usuario": {
+	                        "id": 1,
+	                        "nombre": "Administrador",
+	                        "abreviacion": "A"
+	                    }
+	                },
+	                "receptor": {
+	                    "id": 12,
+	                    "nombre": "cliente",
+	                    "apellido": "prueba",
+	                    "correo_electronico": "c@m.com",
+	                    "fecha_ultimo_acceso": "2014-09-26T22:41:43.804Z",
+	                    "tipo_usuario": {
+	                        "id": 3,
+	                        "nombre": "Usuario Afiliado",
+	                        "abreviacion": "UA"
+	                    }
+	                }
+	            },            
+	        ],
+	        "como_receptor": [
+	            {
+	                "id": 17,
+	                "ancho": 2,
+	                "alto": 2,
+	                "peso": 4,
+	                "costo": 2.35,
+	                "profundidad": 3,
+	                "descripcion": "Test",
+	                "numero_guia": "57921cab6d7d1d1c848747a14336345573dc00fb",
+	                "emisor": {
+	                    "id": 14,
+	                    "nombre": "operador",
+	                    "apellido": "Prueba",
+	                    "correo_electronico": "o@m.com",
+	                    "fecha_ultimo_acceso": "2014-09-26T21:29:39.060Z",
+	                    "tipo_usuario": {
+	                        "id": 2,
+	                        "nombre": "Operador",
+	                        "abreviacion": "O"
+	                    }
+	                },
+	                "receptor": {
+	                    "id": 13,
+	                    "nombre": "Administrador",
+	                    "apellido": "Prueba",
+	                    "correo_electronico": "a@m.com",
+	                    "fecha_ultimo_acceso": "2014-09-27T00:18:14.693Z",
+	                    "tipo_usuario": {
+	                        "id": 1,
+	                        "nombre": "Administrador",
+	                        "abreviacion": "A"
+	                    }
+	                }
+	            }
+	        ]
+	    }
+	}
+
+=end
+
 	def listar_paquete
 		begin
 			prms = params.permit(
@@ -33,7 +128,6 @@ before_filter :check_api_token
 	end
 
 	def buscar_paquete
-	# Buscar paquete por numero de guia	
 		begin
 			prms = params.permit(
 				:numero_guia
--- source/hermes/app/mailers/user_mailer.rb
+++ source/hermes/app/mailers/user_mailer.rb
@@ -1,11 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: 'noreplytohermes@gmail.com'
 
-  def retrieve_pwd(usuario)
-  	@usuario=usuario
-  	mail(to: @usuario.correo_electronico, subject: "Recuperacion de su contrasena")   
-  end
-
   def notificar_receptor(t)
   	@t = t
   	mail to: @t.paquete.receptor.correo_electronico, subject: "Notificacion de llegada de paquete"
